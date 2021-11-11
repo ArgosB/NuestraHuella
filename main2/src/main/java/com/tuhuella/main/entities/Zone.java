054fa1f33003c3e961b66354f13984f73e9cfcca
jramire5 <jjnicoramirez@gmail.com>
2021-11-11 18:31:06 -0300

    Desde el main

--- main2/src/main/java/com/tuhuella/main/entities/Zone.java
+++ main2/src/main/java/com/tuhuella/main/entities/Zone.java
@@ -7,25 +7,32 @@ import javax.persistence.Id;
 public class Zone {
 	@Id
 	private String id;
-	private String name;
-	private String description;
+	private String province;
+	private String city;
+	private String neighborhood;
 	public String getId() {
 		return id;
 	}
 	public void setId(String id) {
 		this.id = id;
+	}			
+	public String getProvince() {
+		return province;
 	}
-	public String getName() {
-		return name;
+	public void setProvince(String province) {
+		this.province = province;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public String getCity() {
+		return city;
 	}
-	public String getDescription() {
-		return description;
+	public void setCity(String city) {
+		this.city = city;
 	}
-	public void setDescription(String description) {
-		this.description = description;
+	public String getNeighborhood() {
+		return neighborhood;
+	}
+	public void setNeighborhood(String neighborhood) {
+		this.neighborhood = neighborhood;
 	}
 	public Zone() {
 		super();
--- main2/src/main/java/com/tuhuella/main/enums/Size.java
+++ main2/src/main/java/com/tuhuella/main/enums/Size.java
@@ -1,4 +1,5 @@
 package com.tuhuella.main.enums;
 
 public enum Size {
+	Small, Medium, Large, Extra_Large
 }
