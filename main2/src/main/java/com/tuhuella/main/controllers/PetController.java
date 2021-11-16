be6c81079e9d16976c447da87e933ff39f8fb47b
Juanjo Dzikiewicz <juanjosedzi@gmail.com>
2021-11-16 20:46:05 -0300

    Update AddAPet form

--- main2/src/main/java/com/tuhuella/main/controllers/PetController.java
+++ main2/src/main/java/com/tuhuella/main/controllers/PetController.java
@@ -9,19 +9,23 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.tuhuella.main.entities.HumanUser;
 import com.tuhuella.main.entities.Photo;
 import com.tuhuella.main.entities.Zone;
 import com.tuhuella.main.enums.Province;
 import com.tuhuella.main.enums.Sex;
 import com.tuhuella.main.enums.Size;
+import com.tuhuella.main.repositories.HumanUserRepository;
 import com.tuhuella.main.repositories.PhotoRepository;
 import com.tuhuella.main.repositories.ZoneRepository;
 import com.tuhuella.main.services.PetService;
+import com.tuhuella.main.services.UserService;
 
 @Controller
 @RequestMapping("/pet")
@@ -30,29 +34,28 @@ public class PetController {
 	@Autowired
 	private PetService petService;
 	@Autowired
+	private HumanUserRepository userRepo;
+	@Autowired
 	private ZoneRepository zoneRepo;
 	@Autowired
 	private PhotoRepository photoRepo;
 
-	@GetMapping("/add-a-pet")
+	@GetMapping("/add-a-pet/{id}")
 	public String form() {
 
 		return "AddAPet-form";
 	}
 
-	@PostMapping("/add-a-pet")
-	public String createPet(ModelMap modelo, @RequestParam("file") MultipartFile file,@RequestParam String name, @RequestParam Integer age, @RequestParam String species,@RequestParam(required=false) String breed, 
+	@PostMapping("/add-a-pet/{id}")
+	public String createPet(ModelMap modelo,@PathVariable String id, @RequestParam("file") MultipartFile file,@RequestParam String name, @RequestParam Integer age, @RequestParam String species,@RequestParam(required=false) String breed, 
 			@RequestParam(required=false) Integer Weight, @RequestParam Sex sex, @RequestParam Size size, @RequestParam Boolean upToDateVaccine, @RequestParam Boolean castrated, @RequestParam Boolean deWormed, @RequestParam String disease, 
 			@RequestParam String city,@RequestParam String country,@RequestParam String neighborhood,@RequestParam Province province) throws Exception {
 
 		try {
 			
-			Zone zone = new Zone();
-			zone.setCity(city);
-			zone.setCountry(country);
-			zone.setNeighborhood(neighborhood);
-			zone.setProvince(province);
-			zoneRepo.save(zone);
+			HumanUser user = userRepo.getById(id);
+			
+			Zone zone = user.getZone();
 			
 
 			Photo photo = new Photo();
@@ -63,6 +66,8 @@ public class PetController {
 			photo.setCreatePhoto(new Date());
 			photoRepo.save(photo);
 			petService.createPet(name, age, species, breed, Weight, sex, size, upToDateVaccine, castrated, deWormed, disease, zone, photo);
+			
+			
 
 			modelo.put("exito", name.toString());
 			return "AddAPet-form";
