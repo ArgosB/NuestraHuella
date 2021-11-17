f3da99719158f4e12e076849e20a504491377ec9
jramire5 <jjnicoramirez@gmail.com>
2021-11-17 09:34:43 -0300

    usercontroller

--- main2/src/main/java/com/tuhuella/main/controllers/UserController.java
+++ main2/src/main/java/com/tuhuella/main/controllers/UserController.java
@@ -1,8 +1,9 @@
 package com.tuhuella.main.controllers;
 
 import java.util.Date;
-import java.util.List;
+//import java.util.List;
 
+import com.tuhuella.main.webException.WebException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -25,7 +26,6 @@ import com.tuhuella.main.services.UserService;
 
 @Controller
 @RequestMapping("/user")
-
 public class UserController {
 	@Autowired
 	private UserService userService;
@@ -47,22 +47,21 @@ public class UserController {
 						   @RequestParam String userName, @RequestParam Long phoneNumber,
 						   @RequestParam(required = false) Long alternativeNumber, @RequestParam String country,
 						   @RequestParam(required = false) Province province, @RequestParam String city,
-						   @RequestParam(required = false) String neighborhood) throws Exception {
+						   @RequestParam(required = false) String neighborhood)  {
 
 		try {
 			
-			/*
-			 * if (password.equals(confirmedPassword)) { throw new
-			 * Exception("Las contraseñas no coinciden"); }
-			 */
+
+			  if (!password.equals(confirmedPassword)) { throw new
+			 Exception("Las contraseñas no coinciden"); }
+
 			 
 			Zone zone = new Zone();
 			zone.setCity(city);
 			zone.setCountry(country);
 			zone.setNeighborhood(neighborhood);
 			zone.setProvince(province);
-			zoneRepo.save(zone);
-
+			zone = zoneRepo.save(zone);
 
 			Photo photo = new Photo();
 			photo.setName(file.getName());
@@ -75,7 +74,7 @@ public class UserController {
 			userService.signUpUser(photo, name, surname, userName, password, birthDate, zone, phoneNumber,
 					alternativeNumber, email);
 
-			modelo.put("exito", name.toString());
+			modelo.put("exito", name);
 			return "singup-form";
 		} catch (Exception e) {
 			modelo.put("error", e.getMessage());
@@ -83,20 +82,22 @@ public class UserController {
 		}
 	}
 
-	@GetMapping("/showUserList")
+	
+	/*@GetMapping("/showUserList")
 	public String show(ModelMap modelo) {
 		List<HumanUser> users = userService.findUsers();
 		modelo.addAttribute("Users", users);
-		return "show-users";
-	}
+		return "UserList";
+	}*/
 
 	@GetMapping("/edit/{id}")
 	public String edit(@PathVariable String id, ModelMap modelo) {
 		HumanUser user = userService.edit(id);
 		modelo.addAttribute("User", user);
-		return "edit-user";
+		return "EditProfile";
 	}
-	@PreAuthorize("hasAnyRol('ROLE_USUARIO_REGISTRADO')")
+
+	
 	@GetMapping("/login")
 	public String login(@RequestParam(required = false) String error, @RequestParam(required = false) String logout, ModelMap model) throws Exception{
 	try {
@@ -106,11 +107,12 @@ public class UserController {
 		if (logout != null) {
 			model.put("logout", "Ha salido correctamente.");
 		}
-		model.put("exito", logout);
 		return "login";
+
 	}catch (Exception e) {
-		model.put("error", e.getMessage());
+		model.put("broken", e.getMessage());
 		return "login";
+
 
 	}
 
